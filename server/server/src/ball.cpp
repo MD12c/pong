40a0163e0edaa820900d8913d7699bbb01201bca
MD12c <danielmilevsky@gmail.com>
2026-05-12 19:00:42 -0400

    fixed nudging in walls

--- server/server/src/ball.cpp
+++ server/server/src/ball.cpp
@@ -23,18 +23,22 @@ namespace Ball {
     
     glm::mat4 translate(GLFWwindow* window, float dT) {
         if(model[3][0] + ballR/2 < -1 || model[3][0] - ballR/2 > 1){/* Game over*/ }
-        if(model[3][1] + ballR/2 <= 1 && model[3][1] - ballR/2 >= -1){
-            float moveX = ball.x * dT;
-            float moveY = ball.y * dT;
-    	    model = glm::translate(model, glm::vec3(moveX, moveY, 0.0f));
-        } else {
-            ball.y = -ball.y;
-            float moveX = ball.x * dT;
-            float moveY = ball.y * dT;
-    	    model = glm::translate(model, glm::vec3(moveX, moveY, 0.0f));
+        float crntY = model[3][1];
+
+        if(crntY + ballR/2 >= 1){
+            ball.y = -std::abs(ball.y);
+            model[3][1] = 1 - ballR/2;
+        } else if(crntY - ballR/2 <= -1){
+            ball.y = std::abs(ball.y);
+            model[3][1] = -1 + ballR/2;
         }
+
+        float moveX = ball.x * dT;
+        float moveY = ball.y * dT;
+        model = glm::translate(model, glm::vec3(moveX, moveY, 0.0f));
         return model;
     }
+    
 
     glm::vec3 getPosition() {
         return glm::vec3(model[3][0], model[3][1], 0.0f);
